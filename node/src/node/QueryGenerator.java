c1dc3c5b3fea3814d7ceb16a4d6ba82c72fff4c0
Channa Gayan <channagayan@gmail.com>
2015-03-05 08:29:06 +0530

    New queries added

--- node/src/node/QueryGenerator.java
+++ node/src/node/QueryGenerator.java
@@ -4,7 +4,15 @@ public class QueryGenerator {
 	public synchronized String getBSRegister(String IP, int port, String username) {
 		return addLength("REG " + IP + " " + port + " " + username);
 	}
-
+	public synchronized String getBSUnRegister(String IP, int port, String username) {
+		return addLength("UNREG " + IP + " " + port + " " + username);
+	}
+	public synchronized String getJOIN(String IP, int port) {
+		return addLength("JOIN " + IP + " " + port);
+	}
+	public synchronized String getLEAVE(String IP, int port) {
+		return addLength("LEAVE " + IP + " " + port);
+	}
 	private String addLength(String str) {
 		int length = str.length() + 6;
 		if (length < 10) {

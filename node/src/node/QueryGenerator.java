c2b8a5ee8366b12bb49f60db161dcb3f01d40466
Channa Gayan <channagayan@gmail.com>
2015-03-05 08:58:38 +0530

    more queries

--- node/src/node/QueryGenerator.java
+++ node/src/node/QueryGenerator.java
@@ -1,5 +1,7 @@
 package node;
 
+import java.util.List;
+
 public class QueryGenerator {
 	public synchronized String getBSRegister(String IP, int port, String username) {
 		return addLength("REG " + IP + " " + port + " " + username);
@@ -10,11 +12,30 @@ public class QueryGenerator {
 	public synchronized String getJoin(String IP, int port) {
 		return addLength("JOIN " + IP + " " + port);
 	}
+	public synchronized String getJoinOK(int value) {
+		return addLength("JOINOK " + value);
+	}
 	public synchronized String getLeave(String IP, int port) {
 		return addLength("LEAVE " + IP + " " + port);
 	}
+	public synchronized String getLeaveOK(int value) {
+		return addLength("LEAVEOK " + value);
+	}
 	public synchronized String getSearch(String IP, int port,String filename,int hops) {
 		return addLength("SER " + IP + " " + port + " " + filename + " " + hops);
+	}
+	public synchronized String getSearchOK(int numberoffiles,String IP, int port,List<String> filenames,int hops) {
+		StringBuffer fileNameString = new StringBuffer();
+		String prefix="";
+		for(String file:filenames){
+			fileNameString.append(prefix);
+			prefix=" ";
+			fileNameString.append(file);
+		}
+		return addLength("SEROK " + filenames.size() + " "+ IP + " " + port + " " + hops + " " + fileNameString.toString() );
+	}
+	public synchronized String getError(String error) {
+		return addLength("ERROR " + error);
 	}
 	private String addLength(String str) {
 		int length = str.length() + 6;

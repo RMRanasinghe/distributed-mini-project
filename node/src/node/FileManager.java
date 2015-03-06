58f5a115fef3cde1d106e319c7dd7e28b2c47bdc
thimalk <tkempitiya@gmail.com>
2015-03-07 00:13:12 +0530

    file search

--- node/src/node/FileManager.java
+++ node/src/node/FileManager.java
@@ -6,27 +6,45 @@ import java.util.List;
 public class FileManager {
 	public final static FileManager INSTANCE = new FileManager();
 	private List<String> files = new LinkedList<String>();
-	
-	private FileManager(){
+
+	public FileManager() {
 	}
-	
-	public synchronized List<String> getFiles(){
+
+	public synchronized List<String> getFiles() {
 		return files;
 	}
-	
-	public synchronized void add(String file){
-		if(!files.contains(file)){
+
+	public synchronized void add(String file) {
+		if (!files.contains(file)) {
 			files.add(file);
 		}
 	}
-	public synchronized void remove(String file){
-		if(file.contains(file)){
+
+	public synchronized void remove(String file) {
+		if (file.contains(file)) {
 			files.remove(file);
 		}
 	}
-	
-	public synchronized LinkedList<String> find(String file){
-		//TODO:search and return all the file names
-		return null;
+
+	public synchronized LinkedList<String> find(String file) {
+		
+		if (files.isEmpty()) {
+			return null;
+		} else {
+			LinkedList<String> result =new LinkedList<String>();
+			for (String afile : files) {
+				int searchMeLength = afile.length();
+				int findMeLength = file.length();
+				for (int i = 0; i <= (searchMeLength - findMeLength); i++) {
+					if (afile.regionMatches(true, i, file, 0, findMeLength)) {
+						result.add(afile);
+						break;
+					}
+				}
+			}
+			return result;
+		}
+
 	}
+
 }

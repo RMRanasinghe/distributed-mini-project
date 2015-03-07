2729ebc50b2e13e90a81d99609e69cf8a491f1b4
Rajitha <rajithayrmr@gmail.com>
2015-03-07 12:00:38 +0530

    Changed message ID buffer from set to queue as set does not preserve order

--- node/src/node/BoundedMessageIDBuffer.java
+++ node/src/node/BoundedMessageIDBuffer.java
@@ -1,13 +1,12 @@
 package node;
 
-import java.util.Iterator;
-import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.Properties;
-import java.util.Set;
+import java.util.Queue;
 
 public class BoundedMessageIDBuffer {
 	private int maxSize;
-	private Set<String> MessageIDSet;
+	private Queue<String> MessageIDSet;
 	private PropertyLoader propertyLoader = new PropertyLoader();
 	private Properties properties = propertyLoader.getProperties();
 	public final static BoundedMessageIDBuffer INSTANCE = new BoundedMessageIDBuffer();
@@ -15,7 +14,7 @@ public class BoundedMessageIDBuffer {
 	private BoundedMessageIDBuffer() {
 		maxSize = Integer.parseInt(properties
 				.getProperty("message.id.buffer.size"));
-		MessageIDSet = new LinkedHashSet<String>();
+		MessageIDSet = new LinkedList<String>();
 	}
 
 	public boolean add(String id) {
@@ -27,9 +26,7 @@ public class BoundedMessageIDBuffer {
 	}
 
 	private void forgetOldestMessageId() {
-		Iterator<String> iter = MessageIDSet.iterator();
-		iter.next();
-		iter.remove();
+		MessageIDSet.remove();
 	}
 
 	@Override

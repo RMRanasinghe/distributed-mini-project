efd3163e8f0e90755d618b2a5d2a81cebf6c6e4b
thimalk <thimal@thimal-Dell-System-Vostro-3450>
2015-04-07 12:42:43 +0530

    change search method to oneway

--- node/queryexecutor.thrift
+++ node/queryexecutor.thrift
@@ -8,6 +8,6 @@ service QueryExecutor
 		void regOKSuccess2 (1:string ip1, 2: int port1, 3:string ip2, 4:int port2),
 		void join(1:string ip, 2:int port),
 		void leave(1:string ip, 2:int port),
-		void fileSearch(1:string fileName, 2:string ip, 3:int port, 4:int id, 5:int hops)
-		void fileFound(1:list<string> fileList, 2:string searchIp, 3:int searchPort,4:string foundIp,5:int foundPort, 6:int id, 7:int hops)
+		oneway void fileSearch(1:string fileName, 2:string ip, 3:int port, 4:int id, 5:int hops)
+		oneway void fileFound(1:list<string> fileList, 2:string searchIp, 3:int searchPort,4:string foundIp,5:int foundPort, 6:int id, 7:int hops)
 }

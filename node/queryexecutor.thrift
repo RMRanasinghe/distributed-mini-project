09189e90f74e60e4a185088601050ef07ee52378
thimalk <thimal@thimal-Dell-System-Vostro-3450>
2015-04-07 14:29:53 +0530

    gona

--- node/queryexecutor.thrift
+++ node/queryexecutor.thrift
@@ -9,5 +9,6 @@ service QueryService
 		string join(1:string ip, 2:int port),
 		string leave(1:string ip, 2:int port),
 		oneway void fileSearch(1:string fileName, 2:string ip, 3:int port, 4:int id, 5:int hops)
+		
 		oneway void fileFound(1:list<string> fileList, 2:string searchIp, 3:int searchPort,4:string foundIp,5:int foundPort, 6:int id, 7:int hops)
 }

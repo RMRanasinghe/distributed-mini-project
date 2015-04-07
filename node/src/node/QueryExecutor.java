a1dc722973f1bcdd1a6eb0850b4ad9b7fac7675d
TharinduAmila <amilastbmmv@gmail.com>
2015-04-07 17:39:28 +0530

    leave using udp

--- node/src/node/QueryExecutor.java
+++ node/src/node/QueryExecutor.java
@@ -38,7 +38,7 @@ public class QueryExecutor {
 		sentIds = BoundedMessageIDBuffer.INSTANCE;
 		BSPort =Integer.parseInt(properties
 				.getProperty("bs.internet.port"));
-		BSIp = properties.getProperty("bs.internet.ip");
+		BSIp = properties.getProperty("bs.internet.address");
 		username = properties.getProperty("node.username");
 	}
 
@@ -127,7 +127,7 @@ public class QueryExecutor {
 		nodeCommunicator.send(ip, port, queryGenerator.getLeave(nodeIP,nodePort));
 	}
 	public void sendLeaveToBS() {
-		nodeCommunicator.send(BSIp, BSPort, queryGenerator.getBSUnRegister(nodeIP, nodePort,username ));
+		nodeCommunicator.sendToBS(BSIp, BSPort, queryGenerator.getBSUnRegister(nodeIP, nodePort,username ));
 	}
 	
 	//Thrift methods
